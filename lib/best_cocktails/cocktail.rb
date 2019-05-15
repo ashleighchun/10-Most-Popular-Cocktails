1cc0d60b834df0843c61dc18c16dd5a1ee43833e
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-05-15 12:15:59 -0700

    add to cocktail.rb accessors

--- lib/best_cocktails/cocktail.rb
+++ lib/best_cocktails/cocktail.rb
@@ -1,14 +1,37 @@
 class BestCocktails::Cocktail
 
-  attr_accessor :name, :url, :description, :ingredients, :recipe
+  attr_accessor :name, :url, :description, :directions, :what_you_need, :prep_time
 
   @@all = []
 
-  def initialize
+  def initialize(name = nil, url = nil, description = nil)
+    @name = name
+    @url = url
+    @description = description
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def self.find(id)
+    self.all[id-1]
+  end
+
+  def directions
+    @directions ||= doc.css("").text
+  end
+
+  def what_you_need
+    @what_you_need ||= doc.css("").text
+  end
+
+  def prep_time
+    @prep_time ||=doc.css("").text
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
   end
 end
